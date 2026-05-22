d5bbbc3f92ef5ae8bfa0a554f8c8092a98996bce
LucieneCavalcanti <lucienecavalcanti@live.com>
2026-05-22 11:58:27 -0300

    Novo modelo de impressão

--- src/entities/Impressao.java
+++ src/entities/Impressao.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 
 public interface Impressao {
     public static final String CABECALHO =
-    "=====  EXERCÍCIOS DO DIA =====\n";
+    "============   EXERCÍCIOS DO DIA  ===========\n";
+    public static final String RODAPE =
+    "=============================================\n"+
+    "Impressão realizada em %s\n" + LocalDate.now();
     public void imprimirTreino();
     public void imprimirTreino(LocalDate data);
 }
